19f00a051247e3911a1b4e389c30f36d4cdf1d00
kakassss <celikonur988@gmail.com>
2024-12-05 01:45:16 +0300

    Missing scripts: those who have forgotten to commit

---
+++ Assets/Scripts/Utils/QuaternionUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class QuaternionUtils
+{
+    public void AimToTarget(Transform transform,Transform target,float rotateSpeed)
+    {
+        if (target == null)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * rotateSpeed);
+            return;
+        }
+        
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, 
+            targetRotation, Time.deltaTime * rotateSpeed);
+    }
+}
