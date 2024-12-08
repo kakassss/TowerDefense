fdb86431ef0eb898264341f5d1f5da4956a0b9ba
kakassss <celikonur988@gmail.com>
2024-12-09 01:10:31 +0300

    Folder Beauty: State Machine folders organized

---
+++ Assets/TestScripts/States/MouseClickBaseState.cs
@@ -0,0 +1,10 @@
+﻿
+public abstract class MouseClickBaseState : State
+{
+    protected readonly MouseClickStateMachine _mouseClickStateMachine;
+    
+    protected MouseClickBaseState(MouseClickStateMachine mouseClickStateMachine)
+    {
+        _mouseClickStateMachine = mouseClickStateMachine;
+    }
+}
