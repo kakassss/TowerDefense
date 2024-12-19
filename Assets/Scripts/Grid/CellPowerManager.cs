d26a6fcdd220a00fb9e27bd22d23b01be22e01ea
kakassss <celikonur988@gmail.com>
2024-12-19 23:29:25 +0300

    EnemyPathFinding: selected column index bug fixed

--- Assets/Scripts/Grid/CellPowerManager.cs
+++ Assets/Scripts/Grid/CellPowerManager.cs
@@ -36,59 +36,103 @@ public class CellPowerManager
         
         return _tempPower;
     }
+    
     //Creating powerSize for each column
+    //1.Step
+    //Find every columnPowerSize
+    //Find total Power Size
+    //Add each one column to (int,float) dictionary 
+    //2.Step
+    //Find each one column power rate
+    //Add to list
+        
+    //PowerSize[0] -> 0, 40
+    //PowerSize[1] -> 1, 80
+    //PowerSize[2] -> 2, 20
     public Dictionary<int, float> GetAllColumnsPowerSize()
     {
         if(_cellManager.GetActiveColumns().Count <= 0) return null;
         
-        _columnPowerRate.Clear();
-        _powerSizeDic.Clear();
-        _totalPower = 0;
-        
-        var activeColumnsCount = _cellManager.GetActiveColumns().Count;
+        var activeColumns = _cellManager.GetActiveColumns();
+        var activeColumnsCount = activeColumns.Count;
 
         if (activeColumnsCount == 1)
         {
             _powerSizeDic.Add(_cellManager.GetActiveColumns()[0], 0);
             return _powerSizeDic;
         }
+
+        // for (int i = 0; i < activeColumnsCount; i++)
+        // {
+        //     Debug.Log("active columnss " + activeColumns[i]);
+        // }
         
-        for (int i = 0; i < activeColumnsCount; i++)
+        _columnPowerRate.Clear();
+        _powerSizeDic.Clear();
+        _totalPower = 0;
+        
+        
+        //Debug.Log("activeColumnsCount " + activeColumnsCount);
+        int index = 0;
+        foreach (var column in activeColumns)
         {
             _tempPower = 0;
             
-            for (int j = 0; j < _cellManager.Width; j++)
+            // We don't check just only active cell, looking every one of them, if its empty then powerRate will be 0
+            for (int j = 0; j < _cellManager.Height; j++)
             {
-                var cell = _cellManager.Grid[j, i];
+                var cell = _cellManager.Grid[j, column];
+                //Debug.Log("current " + j + " "+ cell.Slot.Power);
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                cube.transform.position =
+                    _cellManager.GetCellMidPointPositionXZ(cell.Slot.GridIndexX, cell.Slot.GridIndexZ);
+                
                 _tempPower += cell.Slot.Power;
                 _totalPower += cell.Slot.Power;
             }
-            _powerSizeDic.Add(i, _tempPower);
+            //Debug.Log("current cell " + _tempPower);
+            _powerSizeDic.Add(column, _tempPower);
+            index++;
         }
+        //
+        // for (int i = 0; i < activeColumnsCount; i++)
+        // {
+        //     _tempPower = 0;
+        //     
+        //     // We don't check just only active cell, looking every one of them, if its empty then powerRate will be 0
+        //     for (int j = 0; j < _cellManager.Height; j++)
+        //     {
+        //         var cell = _cellManager.Grid[j, i];
+        //         Debug.Log("current i " + i + cell.Slot.Power);
+        //         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //         cube.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+        //         cube.transform.position =
+        //             _cellManager.GetCellMidPointPositionXZ(cell.Slot.GridIndexX, cell.Slot.GridIndexZ);
+        //         
+        //         _tempPower += cell.Slot.Power;
+        //         _totalPower += cell.Slot.Power;
+        //     }
+        //     Debug.Log("current cell " + _tempPower);
+        //     _powerSizeDic.Add(i, _tempPower);
+        // }
 
-        
-        //1.Step
-        //Find every columnPowerSize
-        //Find total Power Size
-        //Add each one column to (int,float) dictionary 
-        //2.Step
-        //Find each one column power rate
-        //Add to list
-        
-        //PowerSize[0] -> 0, 40
-        //PowerSize[1] -> 1, 80
-        //PowerSize[2] -> 2, 20
-        
-        //Debug.Log("total power " + _totalPower);
-        for (int i = 0; i < _powerSizeDic.Count; i++)
+        //Calculate powerRate for each colomn
+        foreach (var power in _powerSizeDic)
         {
-           var powerRate = Mathf.CeilToInt(_totalPower/ _powerSizeDic[i]) * 10;
-           
-           //Debug.Log(i  + " power rate: " + powerRate);
-           _columnPowerRate.Add(i, powerRate);
+            float powerRate = Mathf.CeilToInt(_totalPower/ power.Value) * 10;
+            _columnPowerRate.Add(power.Key, powerRate);
         }
+        // for (int i = 0; i < _powerSizeDic.Count; i++)
+        // {
+        //     float powerRate = Mathf.CeilToInt(_totalPower/ _powerSizeDic[i]) * 10;
+        //    
+        //     //Debug.Log(i  + " power rate: " + powerRate);
+        //     _columnPowerRate.Add(_powerSizeDic.Keys[i], powerRate);
+        // }
 
         return _columnPowerRate;
+        //Debug.Log("total power " + _totalPower);
     }
     
 
--- Assets/Scripts/WaveSpawn/EnemyWavesPathFinding.cs
+++ Assets/Scripts/WaveSpawn/EnemyWavesPathFinding.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyWavesPathFinding
 {
@@ -37,28 +39,73 @@ public class EnemyWavesPathFinding
         return _cellManager.GetCellMidPointPositionXZ(currentCell.GridIndexX,currentCell.GridIndexZ);
     }
 
+    private Dictionary<int, float> dic = new Dictionary<int, float>();
     public Vector3 GetCalculatedPowerSizeSpawnPoints(Vector3 spawnPointOffset)
     {
-        var dic = _cellPowerManager.GetAllColumnsPowerSize();
-        var keyValuePairs = dic.OrderBy(powerSize => powerSize.Value);
-
+        dic.Clear();
+        
+        dic = _cellPowerManager.GetAllColumnsPowerSize();
+        IOrderedEnumerable<KeyValuePair<int, float>> keyValuePairs;
+        keyValuePairs = dic.OrderBy(powerSize => powerSize.Value);
+        
         if (dic.Count == 1)
         {
             List<int> keyList = new List<int>(dic.Keys);
             return CalculatedSpawnPoint = GetColumnSpawnPoint(keyList[0]) + spawnPointOffset;
         }
-        var random = Random.Range(0, 100);
+        
+        CalculatedSpawnPoint = CalculateSpawnPoint(keyValuePairs, spawnPointOffset);
+        
+        if (CalculatedSpawnPoint == Vector3.zero)
+        {
+            CalculatedSpawnPoint = GetCalculatedPowerSizeSpawnPoints(spawnPointOffset);
+        }
+        // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // cube.transform.localScale = new Vector3(5f,3.5f,0.5f);
+        // cube.transform.position = CalculatedSpawnPoint;
+            
+        //Debug.LogError("Couldn't calculate power spawn points");
+        return CalculatedSpawnPoint;
+    }
 
+    private Vector3 CalculateSpawnPoint(IOrderedEnumerable<KeyValuePair<int, float>> keyValuePairs, Vector3 spawnPointOffset)
+    {
+        var random = Random.Range(0, 100);
+        
+        //TODO SORUN ŞU
+        //4 tane aynı ratede column gelirse hep ilk olan seçiliyor
+        //shuffle tarzı bi şey deneyebilirsin, fakat, bu eğer 3 tane aynı 1 tane farklı gibi olan durumları
+        //nasıl etkiler tam bilemiyorsun
+        //bu guid new guid shuffle yapıyor, fakat bi tık korkuttu, yani stackover flowda çöker cart curt denmiş
+        //https://stackoverflow.com/questions/273313/randomize-a-listt
+        //performans sıkıntısı yaratıyormus
+        keyValuePairs = keyValuePairs.OrderBy(_ => Guid.NewGuid());
         foreach (var column in keyValuePairs)
         {
-            if (random <= column.Value)
+            if (random <= column.Value)// column value is spawn rate
             {
-                //Debug.Log("random " + random +"column hase choosen " + column.Key);
-                return CalculatedSpawnPoint = GetColumnSpawnPoint(column.Key) + spawnPointOffset;
+                Debug.Log("onur ran " + random + " "  + column.Value);
+                return GetColumnSpawnPoint(column.Key) + spawnPointOffset;
             }
         }
-
-        Debug.LogError("Couldn't calculate power spawn points");
         return Vector3.zero;
     }
+    private void ShuffleList<T>(List<T> list)
+    {
+        System.Random random = new System.Random();
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int randomIndex = random.Next(0, i + 1);
+
+            var temp = list[i];
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+    
 }
+
+
+
+
+
