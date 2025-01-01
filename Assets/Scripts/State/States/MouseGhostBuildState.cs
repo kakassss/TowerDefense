3f797449e5780dd0e9a5f44ab489cc6da6331252
kakassss <celikonur988@gmail.com>
2025-01-02 00:51:56 +0300

    CellManager: Grid system building bug fixes and revision

--- Assets/Scripts/State/States/MouseGhostBuildState.cs
+++ Assets/Scripts/State/States/MouseGhostBuildState.cs
@@ -9,10 +9,14 @@ public class MouseGhostBuildState : MouseClickBaseState
 
     private Grid<Cell> _mouseCell;
     private List<Cell> _buildableCells = new List<Cell>();
+    private HashSet<Cell> _cellsSet = new HashSet<Cell>();
     
     private BuildingInputReader _buildingInputReader;
-    
     private MouseClickStateEvents _mouseClickStateEvents;
+    
+    private Vector3 _midPosition;
+    private float _midPosOffset = 3f;
+    private (int x, int z) XZTuple;
     
     public MouseGhostBuildState(MouseClickStateMachine mouseClickStateMachine, GhostObjectReceiver ghostObjectReceiver, CellManager cellManager,
         BuildingInputReader buildingInputReader, MouseClickStateEvents mouseClickStateEvents)
@@ -64,38 +68,39 @@ public class MouseGhostBuildState : MouseClickBaseState
         //_cellManager.GetXZ(_utils.GetValidPositionWithLayerMask(),out var X, out var Z);
         //_cellManager.GetWorldPosition(X, Z);
     }
-
-    private (int x, int z) XZTuple;
     
     private void CalculateGridPos()
     {
         if (_buildableCells != null)
         {
-            
-            
             if (Vector3.Distance(_midPosition, _mouseClickStateMachine.Utils.GetValidPositionWithLayerMask()) <=
                 _midPosOffset)
             {
                 _ghostObjectReceiver.GameObject.transform.position = _midPosition;
-                Debug.Log("onur xd3 " +
-                          Vector3.Distance(_midPosition, _mouseClickStateMachine.Utils.GetValidPositionWithLayerMask()));
+                //Debug.Log("onur xd " + Vector3.Distance(_midPosition, _mouseClickStateMachine.Utils.GetValidPositionWithLayerMask()));
             }
             else
             {
                 CalculateGridPos();
             }
         }
+        else
+        {
+            CalculateGridPos();
+        }
 
         void CalculateGridPos()
         {
-            XZTuple = _cellManager.GetXZ(_mouseClickStateMachine.Utils.GetValidPositionWithLayerMask(),XZTuple.x, XZTuple.z);
+            XZTuple = _cellManager.GetXZ(_mouseClickStateMachine.Utils.GetValidPositionWithLayerMask());
 
             _buildableCells.Clear();
+            _cellsSet.Clear();
 
             for (int i = 0; i < _ghostObjectReceiver.GridIndexX; i++) // object grid size for x
             {
                 for (int j = 0; j < _ghostObjectReceiver.GridIndexZ; j++) // object grid size for z
                 {
+                    
                     if ( XZTuple.x + i >= 0 && XZTuple.z + j >= 0 && XZTuple.x + i < _cellManager.Width && XZTuple.z + j < _cellManager.Height)
                     {
                         var buildCell = _cellManager.Grid[XZTuple.x + i, XZTuple.z + j].Slot;
@@ -106,8 +111,39 @@ public class MouseGhostBuildState : MouseClickBaseState
                             SetMidPosOnGrid(XZTuple.x,XZTuple.z,_ghostObjectReceiver.GhostObjectBuildType());
                             return;
                         }
-                        Debug.Log("onur xd2");            
                         _ghostObjectReceiver.OnGhostMaterialGreenFire();
+                    }
+                    //UP-Right Corner
+                    else if (XZTuple.z + j > _cellManager.Height -1 && XZTuple.x + i > _cellManager.Width -1)
+                    {
+                        var cell1 = _buildableCells[0];
+                        var cell2 = _cellManager.Grid[cell1.GridIndexX - 1, cell1.GridIndexZ - 1].Slot;
+                        var cell3 = _cellManager.Grid[cell1.GridIndexX, cell1.GridIndexZ - 1].Slot;
+                        var cell4 = _cellManager.Grid[cell1.GridIndexX - 1, cell1.GridIndexZ].Slot;
+
+                        _buildableCells.Add(cell2);
+                        _buildableCells.Add(cell3);
+                        _buildableCells.Add(cell4);
+                    }
+                    //UP-Left Corner
+                    else if (XZTuple.z + j > _cellManager.Height -1 && XZTuple.x + i < _cellManager.Width -1)
+                    {
+                        var cell1 = _buildableCells[0];
+                        var cell3 = _cellManager.Grid[cell1.GridIndexX, cell1.GridIndexZ -1].Slot;
+                        var cell4 = _cellManager.Grid[cell3.GridIndexX +1, cell3.GridIndexZ].Slot;
+
+                        _buildableCells.Add(cell3);
+                        _buildableCells.Add(cell4);
+                    }
+                    //DOWN-Right Corner
+                    else if (XZTuple.x + i > _cellManager.Width - 1 && XZTuple.z + j < _cellManager.Height - 1 )
+                    {
+                        var cell1 = _buildableCells[0];
+                        var cell3 = _cellManager.Grid[cell1.GridIndexX -1, cell1.GridIndexZ].Slot;
+                        var cell4 = _cellManager.Grid[cell1.GridIndexX - 1, cell1.GridIndexZ + 1].Slot;
+
+                        _buildableCells.Add(cell3);
+                        _buildableCells.Add(cell4);
                     }
                     else
                     {
@@ -115,8 +151,18 @@ public class MouseGhostBuildState : MouseClickBaseState
                     }
                 }
             }
-
-            SetMidPosMultipleGrid();
+            _cellsSet = _buildableCells.ToHashSet();
+            Debug.Log("asdasd " + _cellsSet.Count);
+            foreach (var cell in _cellsSet)
+            {
+                Debug.Log("sadasd " + cell.GridIndexX + " " + cell.GridIndexZ);
+            }
+            
+            if (_buildableCells.Count != 2 && _buildableCells.Count != 1)
+            {
+                SetMidPosMultipleGrid();
+                _ghostObjectReceiver.GameObject.transform.position = _midPosition;
+            }
         }
         
         
@@ -140,7 +186,7 @@ public class MouseGhostBuildState : MouseClickBaseState
         
     private void SetMidPosMultipleGrid()
     {
-        List<Vector3> cellsPosition = _buildableCells.Select(cell => _cellManager.GetCellMidPointPositionXZ(cell.GridIndexX, cell.GridIndexZ)).ToList();
+        List<Vector3> cellsPosition = _cellsSet.Select(cell => _cellManager.GetCellMidPointPositionXZ(cell.GridIndexX, cell.GridIndexZ)).ToList();
 
         Vector3 averageX = Vector3.zero;
         Vector3 averageZ = Vector3.zero;
@@ -152,17 +198,6 @@ public class MouseGhostBuildState : MouseClickBaseState
         }
         //_ghostObjectReceiver.GameObject.transform.position = new Vector3(averageX.x / cellsPosition.Count, 0, averageZ.z / cellsPosition.Count);
         _midPosition = new Vector3(averageX.x / cellsPosition.Count, 0, averageZ.z / cellsPosition.Count);
-    }
-
-    private Vector3 _midPosition;
-    private float _midPosOffset = 3f;
-    private void SetMidCell()
-    {
-        if (Vector3.Distance(_midPosition, _mouseClickStateMachine.Utils.GetValidPositionWithLayerMask()) <=
-            _midPosOffset)
-        {
-            //_ghostObjectReceiver.GameObject.transform.position = new Vector3(averageX.x / cellsPosition.Count, 0, averageZ.z / cellsPosition.Count);
-        }
     }
     
     private void OnTowerBuildRelease()
